ce71fbd71351a485684550fd3e725f2a4cf70ed0
MiguelG17 <58083387+MiguelG17@users.noreply.github.com>
2021-08-26 19:54:51 +0200

    Hechas logeo y bottom_navigation

--- app/src/main/java/com/example/naruto/MainActivity.java
+++ app/src/main/java/com/example/naruto/MainActivity.java
@@ -2,16 +2,27 @@ package com.example.naruto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.example.naruto.Fragment.AlbumFragment;
+import com.example.naruto.Fragment.ChestFragment;
+import com.example.naruto.Fragment.ShopFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button logout;
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
+
+
 
 
     @Override
@@ -19,14 +30,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        logout = findViewById(R.id.logout);
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                startActivity(new Intent(MainActivity.this, StartActivity.class)
-                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
-            }
-        });
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new AlbumFragment()).commit(); //Se inicia en el fragment que se ponga
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                    switch (menuItem.getItemId()){
+
+                        case R.id.album:
+                            selectedFragment = new AlbumFragment();
+                            break;
+
+                        case R.id.chest:
+                            selectedFragment = new ChestFragment();
+                            break;
+
+                        case R.id.shop:
+                        selectedFragment = new ShopFragment();
+                            break;
+
+                    }
+
+                    if(selectedFragment != null){ //Creo que aquí se puede iniciar el selector donde se quiera
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    }
+                    return true;
+                }
+            };
 }
